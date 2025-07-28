af05e92e4a3fb6f6c66dee12f54d34a4298762f2
Siraj Ahmed <sirajyamin.dev@gmail.com>
2025-07-28 21:48:01 +0500

    home page improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import { Header } from '@/components/header';
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen max-w-5xl mx-auto">
+    <main className="flex flex-col h-screen max-w-5xl mx-auto px-4 bg-background">
       <Header />
-      <ChatWindow />
+      <section className="flex-1 flex flex-col justify-center">
+        <ChatWindow />
+      </section>
     </main>
   );
 }
