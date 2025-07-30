e14e3ac88bb7673a08012cedc5211656a479d99d
Siraj Ahmed <166845007+sirajahmedx@users.noreply.github.com>
2025-07-31 01:31:41 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/components/header';
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen max-w-5xl mx-auto px-4 bg-background">
+    <main className="flex flex-col h-screen max-w-5xl mx-auto px-6 bg-background">
       <Header />
       <section className="flex-1 flex flex-col justify-center">
         <ChatWindow />
